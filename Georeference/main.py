ef35808394ed79e4ca1d3ada7d2e6c87e7da805e
Pedro-080 <37596156+Pedro-080@users.noreply.github.com>
2024-07-04 07:58:31 -0300

    install exif

--- Georeference/main.py
+++ Georeference/main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+from exif import Image
+
+def add_gps_info(file_name, lat, lon):
+    with open(file_name, 'rb') as img_file:
+        img = Image(img_file)
+    
+    img.gps_latitude = (lat, 0, 0)
+    img.gps_latitude_ref = 'N' if lat >= 0 else 'S'
+    img.gps_longitude = (lon, 0, 0)
+    img.gps_longitude_ref = 'E' if lon >= 0 else 'W'
+    
+    with open(file_name, 'wb') as new_img_file:
+        new_img_file.write(img.get_file())
+
+# Exemplo de uso
+add_gps_info('minha_foto.jpg', 37.7749, -122.4194)
